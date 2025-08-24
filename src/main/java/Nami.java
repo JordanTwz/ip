a2e31e9a36e7d145247659e51c131aa935315e0b
JordanTwz <jordtwz@gmail.com>
2025-08-24 22:27:31 +0800

    Nami.java: Add Level-0 greet and exit

---
+++ src/main/java/Nami.java
@@ -0,0 +1,11 @@
+public class Nami {
+    public static void main(String[] args) {
+        String line = "________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Nami.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
