22196f6b72736e53df888e2884e83bb94d47b95e
JordanTwz <jordtwz@gmail.com>
2025-08-28 17:55:18 +0800

    Navi.java: Implement Level-2 add and list

--- src/main/java/Nami.java
+++ src/main/java/Nami.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class Nami {
     private static final String LINE =
             "________________________________________________";
+    private static final int MAX_TASKS = 100;
 
     public static void main(String[] args) {
+        String[] tasks = new String[MAX_TASKS];
+        int count = 0;
+
         Scanner sc = new Scanner(System.in);
         System.out.println(LINE);
         System.out.println("Hello! I'm Nami.");
@@ -18,10 +22,21 @@ public class Nami {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(LINE);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(LINE);
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(LINE);
+            } else {
+                // adding a task
+                if (count < MAX_TASKS) {
+                    tasks[count++] = input;
+                    System.out.println(LINE);
+                    System.out.println("added: " + input);
+                    System.out.println(LINE);
+                }
             }
-            System.out.println(LINE);
-            System.out.println(" " + input);
-            System.out.println(LINE);
         }
     }
 }
