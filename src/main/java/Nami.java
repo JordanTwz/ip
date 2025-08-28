5a842af8fea60dfff10f116e607b407811f36e1e
JordanTwz <jordtwz@gmail.com>
2025-08-28 17:42:42 +0800

    Nami.java: Implement Level-1 echo

--- src/main/java/Nami.java
+++ src/main/java/Nami.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Nami {
+    private static final String LINE =
+            "________________________________________________";
+
     public static void main(String[] args) {
-        String line = "________________________________________________";
-        System.out.println(line);
+        Scanner sc = new Scanner(System.in);
+        System.out.println(LINE);
         System.out.println("Hello! I'm Nami.");
         System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(LINE);
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine().trim();
+            if (input.equals("bye")) {
+                System.out.println(LINE);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+            System.out.println(LINE);
+            System.out.println(" " + input);
+            System.out.println(LINE);
+        }
     }
 }
